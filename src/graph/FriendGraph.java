27ff3ac4d0d87ee777893e6349222f7730f3da85
frankgiordano <giofr01@yahoo.com>
2016-05-03 00:45:49 -0400

    First commit

---
+++ src/graph/FriendGraph.java
@@ -0,0 +1,42 @@
+package graph;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+
+public interface FriendGraph {
+	
+	public Map<Integer, FriendNode> getFriends();
+	
+    /* Creates a vertex with the given number id. */
+    public void addVertex(int num);
+    
+    /* Creates an edge from the first vertex to the second. */
+    public void addEdge(int from, int to);
+    
+    /* Return the graph's connections in a readable format. 
+     * The keys in this HashMap are the vertices in the graph.
+     * The values are the nodes that are reachable via a directed
+     * edge from the corresponding key. 
+	 * The returned representation ignores edge weights and 
+	 * multi-edges.  */
+    public HashMap<Integer, HashSet<Integer>> exportGraph();
+	
+	/* find friends of friends not linked for friend recommendation */
+	public HashMap<Integer, ArrayList<Integer>> suggestFriendsOfFriends(FriendNode person);
+    
+    /*Generate string representation of adjacency list */
+    public String adjacencyString();
+
+    /* export Top number of network by capacity */
+	public List<FriendGraph> exportTopDegreeGraphs(int number);
+
+	public void measureAndSetClosenessCentrality();
+
+	public void measureAndSetBetweennessCentrality();
+
+	public List<FriendNode> returnTopCentralityFor(int number, String string);
+
+}
---
+++ src/graph/FriendNode.java
@@ -0,0 +1,80 @@
+package graph;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class FriendNode implements Comparable<FriendNode> {
+	
+	private int value;
+	private Set<FriendNode> edges;
+	private double closenessCentrality;
+	private double betweennessValue;
+
+	public FriendNode(int value) {
+		if (value < 0) throw new IllegalArgumentException("Number must be 0 or greater.");
+		this.value = value;
+		this.edges = new HashSet<FriendNode>();
+		this.setBetweennessValue(0);
+	}
+	
+	public boolean addEdge(FriendNode node) {
+		// add a friend 
+		if (!edges.contains(node)) {
+			edges.add(node);
+			return true;
+		}
+		
+		return false;
+	}
+	
+	public int getValue() {
+		return value;
+	}
+	
+	public void setValue(int value) {
+		this.value = value;
+	}
+	
+	public Set<FriendNode> getEdges() {
+		return edges;
+	}
+	
+	public void setEdges(Set<FriendNode> edges) {
+		this.edges = edges;
+	}
+	
+	public int getSize() {
+		return this.edges.size();
+	}
+
+	public int getDegreeOfCentrality() {
+		return this.edges.size();
+	}
+	
+	public double getClosenessCentrality() {
+		return closenessCentrality;
+	}
+
+	public void setClosenessCentrality(double closenessCentrality) {
+		this.closenessCentrality = closenessCentrality;
+	}
+
+	@Override
+	public int compareTo(FriendNode o) {
+	    if (this.getDegreeOfCentrality() < o.getDegreeOfCentrality())
+	       return 1;
+	    if (this.getDegreeOfCentrality() > o.getDegreeOfCentrality())
+	       return -1;
+	    return 0;
+		
+	}
+
+	public double getBetweennessValue() {
+		return betweennessValue;
+	}
+
+	public void setBetweennessValue(double betweennessValue) {
+		this.betweennessValue = betweennessValue;
+	}
+
+}
